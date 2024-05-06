54d22ec15a60cdf61cab833526297af558e90e0d
DTVoidberg <davidmabbitt1994@gmail.com>
2024-05-06 20:59:17 +0200

    Add files via upload

---
+++ Day3CollatzConjecture/Day3CollatzConjecture/Program.cs
@@ -0,0 +1,29 @@
+﻿
+class CollatzConjecture
+{
+
+    public static void Main(string[] args)
+    {
+        int userNumber;
+        Console.WriteLine("Welcome to the Collatz Conjecture \nPlease provide a number: ");
+        userNumber = int.Parse(Console.ReadLine()!);
+        Collatz(userNumber);
+
+        Console.WriteLine(Collatz(userNumber));
+
+    }
+    private static int Collatz(int v)
+    {
+        if (v == 1)
+        {
+            return v;
+        }
+        if (v % 2 == 0)
+        {
+            return Collatz(v / 2);
+        }
+        else
+            return Collatz(v *3 + 1);
+
+    }
+}
