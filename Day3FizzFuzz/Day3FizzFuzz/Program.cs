d9956d7742784f726508c1140d69b5cd1bea9fea
DTVoidberg <davidmabbitt1994@gmail.com>
2024-05-06 21:00:31 +0200

    Add files via upload

---
+++ Day3FizzFuzz/Day3FizzFuzz/Program.cs
@@ -0,0 +1,34 @@
+﻿class FizzBuzz
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to FizzBuzz \nPlease enter a number: ");
+        string? userInput = Console.ReadLine();
+        int.TryParse(userInput, out int userNumber);
+
+        for (int i = 1; i <= userNumber; i++)
+        {
+
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (i % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else if (i % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+
+
+    }
+
+}
